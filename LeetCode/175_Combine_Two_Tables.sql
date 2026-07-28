e0250dece92ffcb9c6233313c750be4ab4f2a922
Binary Bane <43386254+safzo@users.noreply.github.com>
2026-07-28 16:27:09 +0600

    Delete LeetCode/175_Combine_Two_Tables.sql

--- LeetCode/175_Combine_Two_Tables.sql
+++
@@ -1,9 +0,0 @@
-/* Write your T-SQL query statement below */
-SELECT 
-    p.firstName, 
-    p.lastName, 
-    q.city, 
-    q.state
-FROM Person AS p
-LEFT JOIN Address AS q 
-    ON p.personId = q.personId;
