8ee6825c170c2ede14f1c8a1b0e927aec0c2fd13
Binary Bane <43386254+safzo@users.noreply.github.com>
2026-08-01 11:53:58 +0600

    Update 183_Customers_Who_Never_Order.sql

--- LeetCode/Free SQL questions (Copy-m8baczxh)/183_Customers_Who_Never_Order.sql
+++ LeetCode/Free SQL questions (Copy-m8baczxh)/183_Customers_Who_Never_Order.sql
@@ -1,5 +1,11 @@
 /* Write your T-SQL query statement below */
+/* Solving this using the join method: */
 SELECT p.name as Customers
 FROM Customers as p
 LEFT JOIN Orders as q ON p.id = q.customerId
 WHERE (q.customerId IS NULL);
+
+/* Solving this using the NOT IN method: */
+SELECT name as Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerid FROM Orders WHERE customerId IS NOT NULL);
